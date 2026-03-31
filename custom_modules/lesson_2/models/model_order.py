0b4877aae1f228dc5fef22f8016b3769b265f3dd
Roman <velp5000@gmail.com>
2026-04-01 00:01:22 +0300

    [IMP] lesson_2: Lesson 2 Added the solution of the second task

--- custom_modules/lesson_2/models/model_order.py
+++ custom_modules/lesson_2/models/model_order.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ModelOrder(models.Model):
@@ -6,12 +6,18 @@ class ModelOrder(models.Model):
     _description = "Main Model"
 
     order_no = fields.Char(string="Order Number")
+    delivery_cost = fields.Float(string="Delivery Cost")
+    global_discount = fields.Float(string="Global Discount")
+    final_amount = fields.Float(string="Final Amount", compute="_compute_final_amount", store=True)
+
     line_ids = fields.One2many(
         "model.order.line",
         "order_id",
         string="Order Lines",
     )
 
-
-
+    @api.depends("line_ids.subtotal", "delivery_cost", "global_discount")
+    def _compute_final_amount(self):
+        for order in self:
+            order.final_amount = (sum(order.line_ids.mapped('subtotal')) + order.delivery_cost) * (1 - order.global_discount / 100)
 
--- custom_modules/lesson_2/models/model_order_line.py
+++ custom_modules/lesson_2/models/model_order_line.py
@@ -8,11 +8,14 @@ class ModelOrderLine(models.Model):
     product_name = fields.Char(string="Product Name")
     unit_price = fields.Float(string="Unit Price")
     count = fields.Integer(string="Count")
-    subtotal = fields.Float(string="Subtotal", store=True)
+    discount = fields.Float(string="Discount")
+    subtotal = fields.Float(string="Subtotal", compute="_compute_subtotal", store=True)
 
     order_id = fields.Many2one("model.order", string="Order", ondelete="cascade")
 
-    @api.depends("unit_price", "count")
+
+    @api.depends("unit_price", "count", "discount")
     def _compute_subtotal(self):
         for line in self:
-            line.subtotal = line.unit_price * line.count
+            line.subtotal = line.unit_price * line.count * (1 - line.discount / 100)
+
